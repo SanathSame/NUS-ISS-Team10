cc6b86f5e69ff36ecf2ecea6f6ac37fb000b22b9
theopin <theopinto_tg104@hotmail.com>
2024-01-27 12:26:17 +0800

    Remodel some frontend stuff

--- frontend/src/features/dashboard/sidebar/Sidebar.tsx
+++ frontend/src/features/dashboard/sidebar/Sidebar.tsx
@@ -25,11 +25,27 @@ export function Sidebar (): JSX.Element {
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link d-flex align-items-center gap-2" href="/orders">
+                            <a className="nav-link d-flex align-items-center gap-2" href="/itineraries">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark" viewBox="0 0 16 16">
                                     <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
                                 </svg>
-                                <strong>Orders</strong>
+                                <strong>Itineraries</strong>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link d-flex align-items-center gap-2" href="/flights">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark" viewBox="0 0 16 16">
+                                    <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
+                                </svg>
+                                <strong>Flights</strong>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link d-flex align-items-center gap-2" href="/hotels">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark" viewBox="0 0 16 16">
+                                    <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
+                                </svg>
+                                <strong>Hotels</strong>
                             </a>
                         </li>
                         <li className="nav-item">
@@ -40,25 +56,10 @@ export function Sidebar (): JSX.Element {
                                 <strong>Products</strong>
                             </a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link d-flex align-items-center gap-2" href="/customers">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-people" viewBox="0 0 16 16">
-                                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
-                                </svg>
-                                <strong>Customers</strong>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link d-flex align-items-center gap-2" href="/reports">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-graph-up" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M0 0h1v15h15v1H0zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07Z" />
-                                </svg>
-                                <strong>Reports</strong>
-                            </a>
-                        </li>
                     </ul>
 
                     <hr className="my-3" />
+                    
                     <ul className="nav flex-column mb-auto">
                         <li className="nav-item">
                             <a className="nav-link d-flex align-items-center gap-2" href="/settings">
--- frontend/src/features/welcome/WelcomeScreen.tsx
+++ frontend/src/features/welcome/WelcomeScreen.tsx
@@ -10,8 +10,8 @@ export function WelcomeScreen (): JSX.Element {
 
           <div className="row align-items-center g-lg-5 py-5">
             <div className="col-lg-7 text-center text-lg-start">
-              <h1 className="display-4 fw-bold lh-1 text-body-emphasis mb-3">Welcome to Boilerplate</h1>
-              <p className="col-lg-10 fs-4">An avenue for you to implement your imaginations and realize your dreams.</p>
+              <h1 className="display-4 fw-bold lh-1 text-body-emphasis mb-3">Welcome to TravelAid</h1>
+              <p className="col-lg-10 fs-4">An avenue for you to make your travel aspirrations a reality.</p>
             </div>
             <div className="col-md-10 mx-auto col-lg-5">
                 <div className="p-4 p-md-5 border rounded-3 bg-body-tertiary">
