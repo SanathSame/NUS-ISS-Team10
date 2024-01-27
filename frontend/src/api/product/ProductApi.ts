e86be62ffb9591a42dfd4fea695dca90413cdd76
theopin <theopinto_tg104@hotmail.com>
2024-01-27 11:06:09 +0800

    Add refactors to user endpoint

--- frontend/src/api/product/ProductApi.ts
+++ frontend/src/api/product/ProductApi.ts
@@ -1,30 +1,33 @@
 import { type ProductFilter } from '../../models/product/ProductFilter'
 import { RequestTypes } from '../utils/RequestTypes'
 
+
+const productEndpoint = '/products/'
+
 const createNewProduct = async (entityDetails: any): Promise<any> => {
-  return await RequestTypes.postRequest('/product/', entityDetails)
+  return await RequestTypes.postRequest(productEndpoint, entityDetails)
 }
 
 const getProducts = async (): Promise<any> => {
-  return await RequestTypes.getRequest('/product/')
+  return await RequestTypes.getRequest(productEndpoint)
 }
 
 const getProductById = async (entityId: any): Promise<any> => {
-  return await RequestTypes.getRequest(`/product/${entityId}`)
+  return await RequestTypes.getRequest(`${productEndpoint + entityId}`)
 }
 
 const getProductsByFilter = async (filterParams: ProductFilter): Promise<any> => {
-  return await RequestTypes.getRequest('/product/', {
+  return await RequestTypes.getRequest(productEndpoint, {
     params: filterParams
   })
 }
 
 const updateProductById = async (id: any, updatedDetails: any): Promise<any> => {
-  return await RequestTypes.patchRequest(`/product/${id}`, updatedDetails)
+  return await RequestTypes.patchRequest(`${productEndpoint + id}`, updatedDetails)
 }
 
 const deleteProductById = async (id: any): Promise<any> => {
-  return await RequestTypes.deleteRequest(`/product/${id}`)
+  return await RequestTypes.deleteRequest(`${productEndpoint +id}`)
 }
 
 export const ProductApi = {
