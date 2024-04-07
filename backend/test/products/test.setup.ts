04d7118f28ca200a211524a64785b0932f72b76b
theopin <theopinto_tg104@hotmail.com>
2024-04-07 20:31:41 +0800

    Fix stuf

--- backend/test/products/test.setup.ts
+++ backend/test/products/test.setup.ts
@@ -1,15 +1,11 @@
 import { app } from '../index'
-import { SampleEntities } from './data/entities.sample'
 
-import mongoose from 'mongoose'
 const port = 4010
 
 let defaultEntityId: string = ''
 let server: any
-let ModelObject: any
 
 const beforeAllFunction = beforeAll(() => {
-  ModelObject = mongoose.models.Product
 
   server = app.listen(port, () => {
     console.log(`[test]: Server is running at http://localhost:${port}`)
@@ -17,18 +13,13 @@ const beforeAllFunction = beforeAll(() => {
 })
 
 const beforeEachFunction = beforeEach(async () => {
-  const modelInstances = SampleEntities.map(data => new ModelObject(data))
 
-  const saveResult = await ModelObject.insertMany(modelInstances)
-  defaultEntityId = saveResult[0]._id
 })
 
 const afterEachFunction = afterEach(async () => {
-  await ModelObject.deleteMany({})
 })
 
 const afterAllFunction = afterAll(async () => {
-  await mongoose.disconnect()
   await server.close()
 })
 
