30cd0f465cd71be8430f3f149df26b4cd9683803
mrwsy1 <chivalrysky@gmail.com>
2024-04-07 16:03:09 +0800

    add fe api

---
+++ frontend/src/api/flight/FlightApi.js
@@ -0,0 +1,38 @@
+import { RequestTypes } from '../utils/RequestTypes'
+
+const flightEndpoint = '/flights/'
+
+const createNewFlight = async (entityDetails) => {
+  return await RequestTypes.postRequest(flightEndpoint, entityDetails)
+}
+
+const getFlights = async () => {
+  return await RequestTypes.getRequest(flightEndpoint)
+}
+
+const getFlightById = async (entityId) => {
+  return await RequestTypes.getRequest(`${flightEndpoint + entityId}`)
+}
+
+const getFlightsByFilter = async (filterParams) => {
+  return await RequestTypes.getRequest(flightEndpoint, {
+    params: filterParams
+  })
+}
+
+const updateFlightById = async (id, updatedDetails) => {
+  return await RequestTypes.patchRequest(`${flightEndpoint + id}`, updatedDetails)
+}
+
+const deleteFlightById = async (id) => {
+  return await RequestTypes.deleteRequest(`${flightEndpoint + id}`)
+}
+
+export const ProductApi = {
+  createNewFlight,
+  getFlights,
+  getFlightById,
+  getFlightsByFilter,
+  updateFlightById,
+  deleteFlightById
+}
