aed9b4dc0f3b7c83b994bc3ff28c01f4d937f83f
theopin <theopinto_tg104@hotmail.com>
2024-04-10 13:30:45 +0800

    Add fixes to linting

--- backend/test/products/test.setup.ts
+++ backend/test/products/test.setup.ts
@@ -17,13 +17,15 @@ const beforeEachFunction = beforeEach(async () => {
   "('6591464efc13ae0a31fa20b9', 'Cold Head Congestion Daytime Non-Drowsy', 'Integer a nibh. In quis justo.', 29.61, 95, '2023-12-28 15:56:58')")
   productObject.one('INSERT INTO products (_id, name, description, price, quantity, created_at) VALUES ' +
   "('6591464efc13ae0a31fa20ba', 'North Cough Drop', 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.', 12.37, 9, '2023-08-08 17:50:59')")
-  console.log('SELECT * FROM products')
+  console.log("before each")
+  console.log(productObject.manyOrNone('SELECT * FROM products'))
 })
 
 const afterEachFunction = afterEach(async () => {
   const productObject = app.get('product-database').getDbObject()
   productObject.oneOrNone('DELETE from products RETURNING *')
-  console.log('SELECT * FROM products')
+  console.log("after each")
+  console.log(productObject.manyOrNone('SELECT * FROM products'))
 })
 
 const afterAllFunction = afterAll(async () => {
