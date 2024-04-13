6bcc90a013c69a446379b7e4e12c2fcef922d64f
theopin <theopinto_tg104@hotmail.com>
2024-04-13 09:31:18 +0800

    Rework test structure

---
+++ backend/test/attractions/unit/crud.test.ts
@@ -0,0 +1,89 @@
+import request from 'supertest'
+import { StatusCode } from 'status-code-enum'
+import { SampleEntities } from '../data/entities.sample'
+
+import * as TestSetup from '../../test.setup'
+
+describe('/attractions/', () => {
+  test('returns status code 201 if it accepts a given entity with appropriate details', async () => {
+    const res: any = await request(TestSetup.app)
+      .post('/attractions/')
+      .send({
+        name: 'Attraction',
+        city: 'Singapore',
+        description: 'Integer a nibh. In quis justo.',
+        price: 29.61,
+        ratings: 5,
+        opening_hours: '10:01 PM',
+        type_of_attraction: 'Beaches and Islands'
+      })
+
+    expect(res.statusCode).toEqual(StatusCode.SuccessCreated)
+  })
+
+  test('returns a data of a created object', async () => {
+    const res: any = await request(TestSetup.app)
+      .post('/attractions/')
+      .send({
+        name: 'Attraction',
+        city: 'Singapore',
+        description: 'Integer a nibh. In quis justo.',
+        price: 29.61,
+        ratings: 5,
+        opening_hours: '10:01 PM',
+        type_of_attraction: 'Beaches and Islands'
+      })
+
+    expect(res.body.data._id).toBeDefined()
+  })
+
+  test('returns status code 200 if can fetch a list of existing entities', async () => {
+    const res: any = await request(TestSetup.app)
+      .get('/attractions/')
+
+    expect(res.statusCode).toEqual(StatusCode.SuccessOK)
+  })
+
+  test('returns the entire list of entities present in the collection', async () => {
+    const res: any = await request(TestSetup.app)
+      .get('/attractions/')
+
+    expect(res.body.data.length).toEqual(SampleEntities.length)
+  })
+
+  test('returns status code 200 if it gets a given entity id', async () => {
+    const res: any = await request(TestSetup.app)
+      .get(`/flights/${TestSetup.defaultEntityId}`)
+
+    expect(res.statusCode).toEqual(StatusCode.SuccessOK)
+  })
+
+  test('returns an object containing the attribute of the entity requested', async () => {
+    const res: any = await request(TestSetup.app)
+      .get(`/flights/${TestSetup.defaultEntityId}`)
+
+    expect(res.body.data.quantity).toEqual(SampleEntities[0].quantity)
+  })
+
+  test('returns status code 200 if it updates a given entity with appropriate id and details', async () => {
+    const res: any = await request(TestSetup.app)
+      .patch(`/attractions/${TestSetup.defaultEntityId}`)
+      .send({ city: 'Malaysia' })
+    expect(res.statusCode).toEqual(StatusCode.SuccessOK)
+  })
+
+  test('returns object with updated attribute if updated successfully', async () => {
+    const res: any = await request(TestSetup.app)
+      .patch(`/attractions/${TestSetup.defaultEntityId}`)
+      .send({ city: 'Malaysia' })
+
+    expect(res.body.data.city).toEqual('Malaysia')
+  })
+
+  test('returns status code 200 if it deletes a given entity id', async () => {
+    const res: any = await request(TestSetup.app)
+      .delete(`/attractions/${TestSetup.defaultEntityId}`)
+
+    expect(res.statusCode).toEqual(StatusCode.SuccessOK)
+  })
+})
