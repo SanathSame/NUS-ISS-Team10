7b2478b6ce3604c31ff9652ad90d328bf76c5f28
ANG Ang <liang.ang@ncs.com.sg>
2024-04-14 15:48:24 +0800

    change2

--- backend/src/hotels/models/entity.model.ts
+++ backend/src/hotels/models/entity.model.ts
@@ -6,7 +6,7 @@ const hotelEntityModelName = 'Hotel'
 const hotelEntitySchema = new Schema({
   hotel_name: { type: String, required: true },
   city: { type: String, required: true },
-  country: { type: String, required: true },  
+  country: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: false },
   ratings: { type: Number, required: true },
