1e90c132cf7d561a2542e238a2404de83a6b18f7
theopin <theopinto_tg104@hotmail.com>
2024-04-27 20:39:06 +0800

    Make some amendments

--- backend/scripts/zap-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-docker pull owasp/zap2docker-stable
-docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://github.com/SanathSame/NUS-ISS-Team10/" -l PASS > zap_baseline_report.html
-
-echo $? > /dev/null
